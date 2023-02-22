5c9d9cf6d9a6aa48bbb02f9793daec1f79fb6042
Ha Phi Hung <haphihung55@gmail.com>
2023-02-22 20:45:31 +0700

    Wire buttons to calculation methods

--- calculator.js
+++ calculator.js
@@ -1,3 +1,4 @@
+var _a, _b;
 function add(a, b) {
     return a + b;
 }
@@ -12,35 +13,76 @@ function divide(a, b) {
 }
 function operate(a, b, op) {
     switch (op) {
-        case "add":
+        case "+":
             return add(a, b);
-        case "subtract":
+        case "-":
             return subtract(a, b);
-        case "multiply":
+        case "*":
             return multiply(a, b);
-        case "divide":
+        case "/":
             return divide(a, b);
     }
 }
-var Calculator = /** @class */ (function () {
-    function Calculator() {
-        this.display = "";
-        this.lastOperand = 0;
+class Calculator {
+    constructor() {
+        this._display = "";
+        this.lastOperand = null;
         this.currentOp = null;
     }
-    Calculator.prototype.startOperation = function (op) {
+    get display() {
+        return this._display;
+    }
+    set display(text) {
+        this._display = text;
+        document.querySelector(".display").textContent = this._display;
+    }
+    enterDisplay(digit) {
+        if (digit === "." && this.display.includes("."))
+            return;
+        if (this.display === "0") {
+            this.display = digit;
+        }
+        else {
+            this.display += digit;
+        }
+    }
+    startOperation(op) {
         this.lastOperand = +this.display;
         this.currentOp = op;
-    };
-    Calculator.prototype.calcResult = function () {
-        if (this.currentOp != null) {
-            var currentOperand = +this.display;
-            var result = operate(this.lastOperand, currentOperand, this.currentOp);
+        this.clear();
+    }
+    calcResult() {
+        if (this.currentOp !== null && this.lastOperand !== null) {
+            const currentOperand = +this._display;
+            const result = operate(this.lastOperand, currentOperand, this.currentOp);
             this.display = result.toString();
         }
-    };
-    Calculator.prototype.clear = function () {
-        this.display = "";
-    };
-    return Calculator;
-}());
+    }
+    clear() {
+        if (this.lastOperand !== null) {
+            this.display = "";
+        }
+        else {
+            this.display = "0";
+        }
+    }
+}
+const calculator = new Calculator();
+document.querySelectorAll(".pad-num-btn").forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+        const btn = e.target;
+        calculator.enterDisplay(btn.textContent);
+    });
+});
+document.querySelectorAll(".pad-op-btn").forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+        const btn = e.target;
+        calculator.startOperation(btn.getAttribute("data-op"));
+    });
+});
+(_a = document.querySelector(".pad-calc-btn")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
+    calculator.calcResult();
+});
+(_b = document.querySelector(".clear-btn")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", () => {
+    calculator.clear();
+});
--- calculator.ts
+++ calculator.ts
@@ -1,4 +1,5 @@
-type operator = "add" | "subtract" | "multiply" | "divide";
+type operator = "+" | "-" | "*" | "/";
+type digit = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "0" | ".";
 
 function add(a: number, b: number): number {
   return a + b;
@@ -18,36 +19,87 @@ function divide(a: number, b: number): number {
 
 function operate(a: number, b: number, op: operator) {
   switch (op) {
-    case "add":
+    case "+":
       return add(a, b);
-    case "subtract":
+    case "-":
       return subtract(a, b);
-    case "multiply":
+    case "*":
       return multiply(a, b);
-    case "divide":
+    case "/":
       return divide(a, b);
   }
 }
 
 class Calculator {
-  display = "";
-  lastOperand = 0;
+  _display = "";
+  lastOperand: number | null = null;
   currentOp: operator | null = null;
+
+  constructor() {
+    this.display = "0";
+  }
+
+  get display() {
+    return this._display;
+  }
+
+  set display(text) {
+    this._display = text;
+    (document.querySelector(".display") as HTMLParagraphElement).textContent = this._display;
+  }
+
+  enterDisplay(digit: digit) {
+    if (digit === "." && this.display.includes(".")) return;
+    if (this.display === "0") {
+      this.display = digit;
+    } else {
+      this.display += digit;
+    }
+  }
 
   startOperation(op: operator) {
     this.lastOperand = +this.display;
     this.currentOp = op;
+    this.clear();
   }
 
   calcResult() {
-    if (this.currentOp != null) {
-      const currentOperand = +this.display;
+    if (this.currentOp !== null && this.lastOperand !== null) {
+      const currentOperand = +this._display;
       const result = operate(this.lastOperand, currentOperand, this.currentOp);
       this.display = result.toString();
     }
   }
 
   clear() {
-    this.display = "";
+    if (this.lastOperand !== null) {
+      this.display = "";
+    } else {
+      this.display = "0";
+    }
   }
 }
+
+const calculator = new Calculator();
+
+document.querySelectorAll(".pad-num-btn").forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    const btn = e.target as HTMLButtonElement;
+    calculator.enterDisplay(btn.textContent as digit);
+  });
+});
+
+document.querySelectorAll(".pad-op-btn").forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    const btn = e.target as HTMLButtonElement;
+    calculator.startOperation(btn.getAttribute("data-op") as operator);
+  });
+});
+
+document.querySelector(".pad-calc-btn")?.addEventListener("click", () => {
+  calculator.calcResult();
+});
+
+document.querySelector(".clear-btn")?.addEventListener("click", () => {
+  calculator.clear();
+});
